69dff0a88fcb3e60a86f31ac207d592833eabec0
Nakul Nandhakumar <nakul@nandhakumar.net>
2020-10-14 22:01:03 -0700

    Made basic new GUI

---
+++ src/Projects/CalculatorGUI.java
@@ -0,0 +1,28 @@
+package Projects;
+
+import javax.swing.*;
+
+public class CalculatorGUI {
+    private JPanel panel1;
+    private JButton button_7;
+    private JButton button_4;
+    private JButton button_1;
+    private JButton button_2;
+    private JButton button_5;
+    private JButton button_8;
+    private JButton button_3;
+    private JButton button_6;
+    private JButton button_9;
+    private JButton button_0;
+    private JButton button_plus;
+    private JButton button_minus;
+    private JButton button_mult;
+    private JButton button_div;
+    private JButton button_mod;
+    private JButton button_equals;
+    private JLabel calcArea;
+    private JButton button_power;
+    private JButton button_sin;
+    private JButton button_tan;
+    private JButton button_cos;
+}
